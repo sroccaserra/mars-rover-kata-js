30da1047e13a012316ba4d225582fea225bc978a
Karam <karamflyy@gmail.com>
2021-03-15 12:07:24 +0100

    error before implimenting the move r

--- test.js
+++ test.js
@@ -63,12 +63,14 @@ class Robot {
       this.y = this.y + 1;
     }
     else if (this.direction == "E") {
+      if(this.x === 1) this.x += 5
       this.x = this.x - 1;
     }
     else if (this.direction == "N"){
       this.y = this.y - 1;
     }
     else if (this.direction == "W"){
+      if(this.x === 5) this.x -=  5;
       this.x = this.x + 1;
     }
   }
@@ -168,3 +170,48 @@ it('moves backward when facing north', () => {
   // then
   expect(robot.getPosition()).to.deep.equal({x: 1, y: 1});
 });
+
+it('moves backward when facing west', () => {
+  // given
+  const robot = new Robot(5, 1, "W");
+
+  // when
+  robot.move("b");
+
+  // then
+  expect(robot.getPosition()).to.deep.equal({x: 1, y: 1});
+});
+
+it('moves backward when facing East', () => {
+  // given
+  const robot = new Robot(2, 1, "E");
+
+  // when
+  robot.move("b");
+
+  // then
+  expect(robot.getPosition()).to.deep.equal({x: 1, y: 1});
+});
+
+it('moves backwards when facing East and x = 1', () => {
+  // given
+  const robot = new Robot(1, 1, "E");
+
+  // when
+  robot.move("b");
+
+  // then
+  expect(robot.getPosition()).to.deep.equal({x: 5, y: 1});
+});
+
+it('the robot is facing north , then turns left and moves forward',() => {
+  //given
+  const robot = new Robot(1,1,"N")
+
+  //when
+  robot.move("r");
+  robot.move("f")
+
+  //then
+  expect(robot.getPosition()).to.deep.equal({x: 2, y: 1})
+})
